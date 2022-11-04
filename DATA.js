f07d0cd4c32b7ae29ace5f244da643965b96a9db
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:36:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "2d2c042f-795c-47b3-b0d4-631a58d96a1d",
+    "home_feed": [
+        {
+            "id": "feac1e00-0eb1-4601-b40a-e9452ca33ec8",
+            "artist_name": "between",
+            "home_carousel": [
+                {
+                    "id": "d547280b-550a-45eb-bc45-117be095f5fe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "6debc403-ad40-4096-bb4d-e7215c867938",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "d8aa3db3-b0ae-47ec-bb5a-e49e43d42e9f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "a4d50692-f63d-4c53-98d8-d1888838730f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "0393e5e5-99b2-4813-ad02-35debd8cec19",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+                }
+            ],
+            "art_caption": "stage",
+            "art_description": "Social memory phone issue. Individual low deal. Life level gas these brother know detail everyone."
+        },
+        {
+            "id": "c42e75ed-8f8c-4e60-b508-ff13e48a52aa",
+            "artist_name": "east",
+            "home_carousel": [
+                {
+                    "id": "81b68af2-afbf-403c-ae9c-fd2c75175b55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                },
+                {
+                    "id": "b3a8885f-a244-41c9-a0ad-c53f4887e962",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "157bcaa8-e0e9-48a3-bd9b-ed4d05f5afd3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                },
+                {
+                    "id": "04ec6403-b217-4e21-a36c-36cb2bc505bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                },
+                {
+                    "id": "e4809a75-b9a2-435a-9e0a-f7c28c37a6f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                }
+            ],
+            "art_caption": "let",
+            "art_description": "Of vote compare fill summer role movie. Television across hair computer. Tell agent like special forget."
+        },
+        {
+            "id": "1a637549-76b3-4a1b-9b42-7a4772283c2d",
+            "artist_name": "final",
+            "home_carousel": [
+                {
+                    "id": "898b5566-176a-412a-9782-513c5f0a24f9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/171.png"
+                },
+                {
+                    "id": "167d9c8e-1905-4fd7-89ec-42a6a8fec256",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                },
+                {
+                    "id": "d7a6f9f5-ed0e-455f-8e40-7ad68448cfe7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "d8a6aa2e-4334-4edc-86b0-426620308c89",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "c307fe15-1c5a-4f81-aa0e-f5fa26a2848e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                }
+            ],
+            "art_caption": "character",
+            "art_description": "Adult lead me indeed successful learn degree."
+        },
+        {
+            "id": "53dc9adf-ea46-4982-873d-3ef068a2078f",
+            "artist_name": "nature",
+            "home_carousel": [
+                {
+                    "id": "382c285c-285a-495e-ab90-104c1b9b44e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "93c65c0e-abc5-4d6b-a1b5-a45f78231848",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "01166b6a-2a2b-4c5b-ae45-6109990b70ab",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "a2081b23-c7f7-4432-b3ca-863b9b8dfb1e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "00ccc01e-bd4f-42cc-ac0a-78108cbef74b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png"
+                }
+            ],
+            "art_caption": "for",
+            "art_description": "Million address than occur investment something job wife."
+        },
+        {
+            "id": "183f72a2-be76-4592-bbef-7aa93a10e1b7",
+            "artist_name": "eight",
+            "home_carousel": [
+                {
+                    "id": "bbed5b44-1801-4b48-9e59-3b3560922d82",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "8d961c89-e7ad-4a0a-9d80-3b12c51e61cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "c5d331c0-5b44-485e-bc3f-3a2fb8d00e99",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "69db5444-4afe-48c4-806d-7b8f13051df0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                },
+                {
+                    "id": "901e1c51-6ee4-4225-a0f8-a174f737de6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                }
+            ],
+            "art_caption": "figure",
+            "art_description": "Dark law parent determine color fill defense dream. Foreign heart government color moment. Network family sign yes."
+        }
+    ]
+}
